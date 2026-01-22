49edd8104aad4e4ab10b7c13b3c2d795e952b178
Phiphi67 <91739842+Phiphi67@users.noreply.github.com>
2026-01-22 13:26:13 +0100

    Update temperaturelamp.py

--- temperaturelamp.py
+++ temperaturelamp.py
@@ -1,7 +1,7 @@
 import time, board, adafruit_dht
 from RPi import GPIO
 
-T=22.0; G=23; R=17
+T=22.0; G=27; R=15
 d=adafruit_dht.DHT22(board.D4)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup((G,R), GPIO.OUT, initial=GPIO.LOW)
@@ -10,11 +10,12 @@ try:
     while True:
         try:
             t,h=d.temperature,d.humidity
-            GPIO.output(G, t<=T)
-            GPIO.output(R, t>T)
+            GPIO.output(R, t<=T)
+            GPIO.output(G, t>T)
             print(f"{t:.1f}°C -> {'GREEN' if t<=T else 'RED'}")
             time.sleep(5)
         except RuntimeError:
             time.sleep(2)
 finally:
     GPIO.cleanup()
+
